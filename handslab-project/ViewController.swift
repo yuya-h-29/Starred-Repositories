246c87999d08235f563d27cef827bde51925bfbb
yuya-h-29 <tzou11291996@gmail.com>
2020-10-12 14:18:37 +0900

    Initial Commit

---
+++ handslab-project/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  handslab-project
+//
+//  Created by Yuya Harada on 2020/10/12.
+//  Copyright © 2020 Yuya Harada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
